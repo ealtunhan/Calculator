7a917fbba26d27cdb422ade4996459857e1accce
ealtunhan <41599720+ealtunhan@users.noreply.github.com>
2019-01-19 16:02:57 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
     
     public static double multiply (double num1, double num2){
         return num1 * num2;
-    
+    // I want to do that with only git pull now
     } 
 // just doing something
     // again and again I'm trying
