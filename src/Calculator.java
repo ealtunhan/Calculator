3cbdf5bedbe840b5ef7b3f5deff1d809f54272be
ealtunhan <41599720+ealtunhan@users.noreply.github.com>
2019-01-19 17:01:00 -0500

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,9 +8,7 @@ public class Calculator {
         return num1 - num2;
     }
     public static double divide ( double num1, double num2){
-        return num1 \ num2;
-        // we are studying about merge conflict
-  
+        return num1 \ num2;  
     } 
 // just doing something
     // again and again I'm trying
